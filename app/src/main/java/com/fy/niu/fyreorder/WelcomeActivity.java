8c0d0b0daabc4322ba42a00fb3faba18b752186b
nx6313 <2559635030@qq.com>
2017-04-24 18:04:19 +0800

    FyReorder创建项目

--- app/src/main/java/com/fy/niu/fyreorder/WelcomeActivity.java
+++ app/src/main/java/com/fy/niu/fyreorder/WelcomeActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
