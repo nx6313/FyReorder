0b47986813bf0227d7a8625dcb80ac36d4e4b804
nx6313 <1823052143@qq.com>
2017-08-20 22:30:45 +0800

    增加腾讯推送支持

--- app/src/main/java/com/fy/niu/fyreorder/util/MyApplication.java
+++ app/src/main/java/com/fy/niu/fyreorder/util/MyApplication.java
@@ -1,8 +1,11 @@
 package com.fy.niu.fyreorder.util;
 
 import android.app.Application;
+import android.util.Log;
 
-import cn.jpush.android.api.JPushInterface;
+import com.tencent.android.tpush.XGIOperateCallback;
+import com.tencent.android.tpush.XGPushConfig;
+import com.tencent.android.tpush.XGPushManager;
 
 /**
  * Created by 25596 on 2017/4/18.
@@ -20,8 +23,17 @@ public class MyApplication extends Application {
         super.onCreate();
         instance = this;
 
-        // 初始化极光推送SDK
-        JPushInterface.setDebugMode(true);
-        JPushInterface.init(this);
+        XGPushConfig.enableDebug(getApplicationContext(), true);
+        XGPushManager.registerPush(getApplicationContext(), new XGIOperateCallback() {
+            @Override
+            public void onSuccess(Object data, int i) {
+                Log.d("TPush", "注册成功，设备token为：" + data);
+            }
+
+            @Override
+            public void onFail(Object data, int errCode, String msg) {
+                Log.d("TPush", "注册失败，错误码：" + errCode + ",错误信息：" + msg);
+            }
+        });
     }
 }
