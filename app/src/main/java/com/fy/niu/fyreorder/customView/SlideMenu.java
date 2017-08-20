11a972782ef9965edc675034f38ec07bf2aca24f
nx6313 <1823052143@qq.com>
2017-08-20 19:01:15 +0800

    修改侧边栏滑动方式

--- app/src/main/java/com/fy/niu/fyreorder/customView/SlideMenu.java
+++ app/src/main/java/com/fy/niu/fyreorder/customView/SlideMenu.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.ViewGroup;
