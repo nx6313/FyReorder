945eb34032440d666aef4ba74d66cc3a8dd1b8b4
ngdong <2559635030@qq.com>
2018-03-02 17:52:03 +0800

    11

--- app/src/main/java/com/fy/niu/fyreorder/util/ConnectorInventory.java
+++ app/src/main/java/com/fy/niu/fyreorder/util/ConnectorInventory.java
@@ -15,14 +15,14 @@ public class ConnectorInventory {
      * 用户登录
      */
     public static void  userLogin(Context context, RequestParams params, DisposeDataHandle dataHandle) {
-        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE + "toAppLogin", params), dataHandle);
+        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE_NEW + "toAppLogin", params), dataHandle);
     }
 
     /**
      * 获取用户基本信息数据
      */
     public static void getUserInfo(Context context, RequestParams params, DisposeDataHandle dataHandle) {
-        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE + "getUserDetailMsg", params), dataHandle);
+        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE_NEW + "getUserDetailMsg", params), dataHandle);
     }
 
     /**
@@ -43,49 +43,49 @@ public class ConnectorInventory {
      * 获取接单数据(已接)
      */
     public static void getOrderListReceived(Context context, RequestParams params, DisposeDataHandle dataHandle) {
-        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE + "getOrderListReceived", params), dataHandle);
+        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE_NEW + "getOrderListReceived", params), dataHandle);
     }
 
     /**
      * 获取接单数据(未接)
      */
     public static void getOrderListMissed(Context context, RequestParams params, DisposeDataHandle dataHandle) {
-        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE + "getOrderListMissed", params), dataHandle);
+        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE_NEW + "getOrderListMissed", params), dataHandle);
     }
 
     /**
      * 获取已完成接单数据
      */
     public static void getCompleteOrderList(Context context, RequestParams params, DisposeDataHandle dataHandle) {
-        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE + "getOrderListFinish", params), dataHandle);
+        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE_NEW + "getOrderListFinish", params), dataHandle);
     }
 
     /**
      * 修改订单状态
      */
     public static void updateOrderState(Context context, RequestParams params, DisposeDataHandle dataHandle) {
-        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE + "updateOrderState", params), dataHandle);
+        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE_NEW + "updateOrderState", params), dataHandle);
     }
 
     /**
      * 获取订单金额数据
      */
     public static void getOrderNumChargeByday(Context context, RequestParams params, DisposeDataHandle dataHandle) {
-        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE + "getOrderNumChargeByday", params), dataHandle);
+        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE_NEW + "getOrderNumChargeByday", params), dataHandle);
     }
 
     /**
      * 设置开启或关闭接单
      */
     public static void setUserIfOpen(Context context, RequestParams params, DisposeDataHandle dataHandle) {
-        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE + "toSetUserIfOpen", params), dataHandle);
+        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE_NEW + "toSetUserIfOpen", params), dataHandle);
     }
 
     /**
      * 修改密码
      */
     public static void updateUserPass(Context context, RequestParams params, DisposeDataHandle dataHandle) {
-        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE + "toUpdateUserPass", params), dataHandle);
+        CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE_NEW + "toUpdateUserPass", params), dataHandle);
     }
 
     /**
@@ -106,7 +106,7 @@ public class ConnectorInventory {
      * 获取最后版本数据
      */
     public static Call getNewAppVersion(Context context, RequestParams params, DisposeDataHandle dataHandle) {
-        return CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE + "getNewVersion", params), dataHandle);
+        return CommonOkHttpClient.post(CommonRequest.createGetRequest(context, Constants.HTTP_URL_BASE_NEW + "getNewVersion", params), dataHandle);
     }
 
 }
